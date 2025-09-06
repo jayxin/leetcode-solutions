97668177e6feafb976cdc8d12aa02323e0de5a57
Jay Xin <jayxin3002@qq.com>
2025-09-06 20:26:21 +0800

    Add 3136-valid-word.

---
+++ with-number/3136-valid-word/main.c
@@ -0,0 +1,96 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+// 3136. 有效单 词 - 有效单词 需要满足以下几个条件：
+//  * 至少 包含 3 个字符。
+//  * 由数字 0-9 和英文大小写字母组成。（不必包含所有这类字符。）
+//  * 至少 包含一个 元音字母 。
+//  * 至少 包含一个 辅音字母 。
+// 给你一个字符串 word 。如果 word 是一个有效单词，则返回 true ，否则返回 false 。
+// 注意：
+//  * 'a'、'e'、'i'、'o'、'u' 及其大写形式都属于 元音字母 。
+//  * 英文中的 辅音字母 是指那些除元音字母之外的字母。
+//  
+// 示例 1：
+// 输入：word = "234Adas"
+// 输出：true
+// 解释：
+// 这个单词满足所有条件。
+// 示例 2：
+// 输入：word = "b3"
+// 输出：false
+// 解释：
+// 这个单词的长度少于 3 且没有包含元音字母。
+// 示例 3：
+// 输入：word = "a3$e"
+// 输出：false
+// 解释：
+// 这个单词包含了 '$' 字符且没有包含辅音字母。
+//  
+// 提示：
+//  * 1 <= word.length <= 20
+//  * word 由英文大写和小写字母、数字、'@'、'#' 和 '$' 组成。
+
+// #v1
+bool isType1(char c) {
+  return (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
+}
+
+bool isValid(char* word) {
+  int len = strlen(word);
+  if (len < 3) return false;
+  char *s = word, *e = s + len, c;
+  bool have_type1 = false, have_type2 = false;
+  while (s < e) {
+    c = *s;
+
+    if (isalnum(c)) {
+      if (isalpha(c)) {
+        c = tolower(c);
+
+        if (isType1(c)) {
+          have_type1 = true;
+        } else {
+          have_type2 = true;
+        }
+      }
+    } else {
+      return false;
+    }
+
+    s++;
+  }
+  if (have_type1 && have_type2) return true;
+  return false;
+}
+// #v1
+
+// read one line from stream
+char* read_line(char *buffer, size_t size, FILE *stream) {
+  if (fgets(buffer, size, stream) == NULL) {
+    return NULL;
+  }
+
+  size_t len = strlen(buffer);
+  if (len > 0 && buffer[len - 1] == '\n') {
+    buffer[len - 1] = '\0';
+  }
+
+  return buffer;
+}
+int main(int argc, char *argv[])
+{
+  char str[500];
+  read_line(str, 499, stdin);
+  if (isValid(str)) {
+    printf("true\n");
+  } else {
+    printf("false\n");
+  }
+  return EXIT_SUCCESS;
+}
+
+/* vim: set fdm=marker fmr={,}: */

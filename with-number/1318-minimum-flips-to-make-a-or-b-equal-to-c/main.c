798c234df5eea78fa257bdd7c5732e115396f43d
Jay Xin <jayxin3002@qq.com>
2025-09-16 23:07:40 +0800

    Add 1318-minimum-flips-to-make-a-or-b-equal-to-c.

---
+++ with-number/1318-minimum-flips-to-make-a-or-b-equal-to-c/main.c
@@ -0,0 +1,57 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+// 1318. 或运算的最小翻转次数 - 给你三个正整数 a、b 和 c。
+// 你可以对 a 和 b 的二进制表示进行位翻转操作，返回能够使按位或运算   a OR b == c   成立的最小翻转次数。
+// 「位翻转操作」是指将一个数的二进制表示任何单个位上的 1 变成 0 或者 0 变成 1 。
+//  
+// 示例 1：
+// [https://assets.leetcode-cn.com/aliyun-lc-upload/uploads/2020/01/11/sample_3_1676.png]
+// 输入：a = 2, b = 6, c = 5
+// 输出：3
+// 解释：翻转后 a = 1 , b = 4 , c = 5 使得 a OR b == c
+// 示例 2：
+// 输入：a = 4, b = 2, c = 7
+// 输出：1
+// 示例 3：
+// 输入：a = 1, b = 2, c = 3
+// 输出：0
+//  
+// 提示：
+//  * 1 &lt;= a &lt;= 10^9
+//  * 1 &lt;= b &lt;= 10^9
+//  * 1 &lt;= c &lt;= 10^9
+
+// #v1
+int minFlips(int a, int b, int c) {
+  int ret = 0;
+
+  while (c || a || b) {
+    if (c & 1) {
+      if (!(a & 1) && !(b & 1)) ret += 1;
+    } else {
+      ret += (a & 1);
+      ret += (b & 1);
+    }
+
+    c >>= 1;
+    a >>= 1;
+    b >>= 1;
+  }
+
+  //c==0 a=1 or b=1
+  //c==1 a=0 and b=0
+  return ret;
+}
+// #v1
+
+int main(int argc, char *argv[])
+{
+  int a, b, c;
+  scanf("%d%d%d", &a, &b, &c);
+  printf("%d\n", minFlips(a, b, c));
+  return EXIT_SUCCESS;
+}
+
+/* vim: set fdm=marker fmr={,}: */

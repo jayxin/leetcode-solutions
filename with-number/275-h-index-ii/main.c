a5259b70d53cc1cdfa661d7f72a670d042059e95
Jay Xin <jayxin3002@qq.com>
2025-09-28 11:50:11 +0800

    Add 275-h-index-ii.

---
+++ with-number/275-h-index-ii/main.c
@@ -0,0 +1,64 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+// 275. H 指数 II - 给你一个整数数组 citations ，其中 citations[i] 表示研究者的第 i 篇论文被引用 的次数，citations 已经按照 非降序排列 。计算并返回该研究者的 h 指数。
+// h 指数的定义 [https://baike.baidu.com/item/h-index/3991452?fr=aladdin]：h 代表“高引用次数”（high citations），一名科研人员的 h 指数是指他（她）的 （n 篇论文中）至少 有 h 篇论文分别被引用了至少 h 次。
+// 请你设计并实现对数时间复杂度的算法解决此问题。
+//  
+// 示例 1：
+// 输入：citations = [0,1,3,5,6]
+// 输出：3
+// 解释：给定数组表示研究者总共有 5 篇论文，每篇论文相应的被引用了 0, 1, 3, 5, 6 次 。
+//      由于研究者有3篇论文每篇 至少 被引用了 3 次，其余两篇论文每篇被引用 不多于 3 次，所以她的 h 指数是 3 。
+// 示例 2：
+// 输入：citations = [1,2,100]
+// 输出：2
+//  
+// 提示：
+//  * n == citations.length
+//  * 1 &lt;= n &lt;= 105
+//  * 0 &lt;= citations[i] &lt;= 1000
+//  * citations 按 升序排列
+
+// #v1
+int hIndex(int* citations, int citationsSize) {
+  int l = 0, r = citationsSize - 1, m, remain;
+
+  while (l < r) {
+    m = (l + r) >> 1;
+    remain = citationsSize - m;
+
+    if (citations[m] >= remain) { // condition satisfied
+      r = m;
+    } else {
+      l = m + 1;
+    }
+  }
+
+  remain = citationsSize - l;
+  if (citations[l] >= remain) return remain;
+
+  return 0;
+}
+// #v1
+
+// read 1d array
+int* read_1d_arr(int n) {
+  int *ret = (int *)malloc(sizeof(int) * n);
+  for (int i = 0; i < n; i++) {
+    scanf("%d", ret + i);
+  }
+  return ret;
+}
+
+int main(int argc, char *argv[])
+{
+  int n, *a;
+  scanf("%d", &n);
+  a = read_1d_arr(n);
+  printf("%d\n", hIndex(a, n));
+  return EXIT_SUCCESS;
+}
+
+/* vim: set fdm=marker fmr={,}: */

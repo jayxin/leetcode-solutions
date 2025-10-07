c741bc238cdaf9d3c3f7d0ff51e6e1ecae4010bb
Jay Xin <jayxin3002@qq.com>
2025-10-07 14:52:20 +0800

    Add 2869-minimum-operations-to-collect-elements.

---
+++ with-number/2869-minimum-operations-to-collect-elements/main.c
@@ -0,0 +1,64 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+// 2869. 收集元素的最少操作次数 - 给你一个正整数数组 nums 和一个整数 k 。
+// 一次操作中，你可以将数组的最后一个元素删除，将该元素添加到一个集合中。
+// 请你返回收集元素 1, 2, ..., k 需要的 最少操作次数 。
+//  
+// 示例 1：
+// 输入：nums = [3,1,5,4,2], k = 2
+// 输出：4
+// 解释：4 次操作后，集合中的元素依次添加了 2 ，4 ，5 和 1 。此时集合中包含元素 1 和 2 ，所以答案为 4 。
+// 示例 2：
+// 输入：nums = [3,1,5,4,2], k = 5
+// 输出：5
+// 解释：5 次操作后，集合中的元素依次添加了 2 ，4 ，5 ，1 和 3 。此时集合中包含元素 1 到 5 ，所以答案为 5 。
+// 示例 3：
+// 输入：nums = [3,2,5,3,1], k = 3
+// 输出：4
+// 解释：4 次操作后，集合中的元素依次添加了 1 ，3 ，5 和 2 。此时集合中包含元素 1 到 3  ，所以答案为 4 。
+//  
+// 提示：
+//  * 1 &lt;= nums.length &lt;= 50
+//  * 1 &lt;= nums[i] &lt;= nums.length
+//  * 1 &lt;= k &lt;= nums.length
+//  * 输入保证你可以收集到元素 1, 2, ..., k 。
+
+// #v1
+int minOperations(int* nums, int numsSize, int k) {
+  int64_t std = 0LL, set = 0LL;
+  int c = 0;
+
+  for (int i = 1; i <= k; i++) std |= 1LL << i;
+
+  for (int i = numsSize - 1; i >= 0; i--) {
+    ++c;
+    if (nums[i] <= k) set |= 1LL << nums[i];
+    if (set == std) break;
+  }
+
+  return c;
+}
+// #v1
+
+// read 1d array
+int* read_1d_arr(int n) {
+  int *ret = (int *)malloc(sizeof(int) * n);
+  for (int i = 0; i < n; i++) {
+    scanf("%d", ret + i);
+  }
+  return ret;
+}
+
+int main(int argc, char *argv[])
+{
+  int n, *a, k;
+  scanf("%d", &n);
+  a = read_1d_arr(n);
+  scanf("%d", &k);
+  printf("%d\n", minOperations(a, n, k));
+  return EXIT_SUCCESS;
+}
+
+/* vim: set fdm=marker fmr={,}: */

348272caee1794e341ff3e1656f62ccfd099d150
Jay Xin <jayxin3002@qq.com>
2025-10-14 16:54:17 +0800

    Add 841-keys-and-rooms.

---
+++ with-number/841-keys-and-rooms/main.c
@@ -0,0 +1,90 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// 841. 钥匙和 房间 - 有 n 个房间，房间按从 0 到 n - 1 编号。最初，除 0 号房间外的其余所有房间都被锁住。你的目标是进入所有的房间。然而，你不能在没有获得钥匙的时候进入锁住的房间 。
+// 当你进入一个房间，你可能会在里面找到一套 不同的钥匙，每把钥匙上都有对应的房间号，即表示钥匙可以打开的房间。你可以拿上所有钥匙去解锁其他房间。
+// 给你一个数组 rooms 其中 rooms[i] 是你进入 i 号房间可以获得的钥匙集合。如果能进入 所有 房间返回 true，否则返回 false。
+//  
+// 示例 1：
+// 输入：rooms = [[1],[2],[3],[]]
+// 输出：true
+// 解释：
+// 我们从 0 号房间开始，拿到钥匙 1。
+// 之后我们去 1 号房间，拿到钥匙 2。
+// 然后我们去 2 号房间，拿到钥匙 3。
+// 最后我们去了 3 号房间。
+// 由于我们能够进入每个房间，我们返回 true。
+// 示例 2：
+// 输入：rooms = [[1,3],[3,0,1],[2],[0]]
+// 输出：false
+// 解释：我们不能进入 2 号房间。
+//  
+// 提示：
+//  * n == rooms.length
+//  * 2 <= n <= 1000
+//  * 0 <= rooms[i].length <= 1000
+//  * 1 <= sum(rooms[i].length) <= 3000
+//  * 0 <= rooms[i][j] < n
+//  * 所有 rooms[i] 的值 互不相同
+
+// #v1
+int cnt, **a, *c;
+char *v;
+
+void dfs(int pos) {
+  v[pos] = 1;
+  ++cnt;
+
+  for (int i = 0; i < c[pos]; i++) {
+    if (!v[a[pos][i]]) dfs(a[pos][i]);
+  }
+}
+
+bool canVisitAllRooms(int** rooms, int roomsSize, int* roomsColSize) {
+  cnt = 0;
+  a = rooms;
+  c = roomsColSize;
+
+  char visited[roomsSize];
+
+  memset(visited, 0, roomsSize * sizeof(char));
+  v = visited;
+
+  dfs(0);
+
+  if (cnt == roomsSize) return true;
+
+  return false;
+}
+// #v1
+
+int main(int argc, char *argv[])
+{
+  int n, *cols, **a;
+  scanf("%d", &n);
+  a = (int **)malloc(sizeof(int *) * n);
+  cols = (int *)malloc(sizeof(int) * n);
+
+  for (int i = 0; i < n; i++) {
+    scanf("%d", cols + i);
+    if (cols[i] == 0) {
+      a[i] = NULL;
+      continue;
+    }
+    a[i] = (int *)malloc(sizeof(int) * cols[i]);
+    for (int j = 0; j < cols[i]; j++) {
+      scanf("%d", &a[i][j]);
+    }
+  }
+
+  if (canVisitAllRooms(a, n, cols)) {
+    printf("true\n");
+  } else {
+    printf("false\n");
+  }
+  return EXIT_SUCCESS;
+}
+
+/* vim: set fdm=marker fmr={,}: */

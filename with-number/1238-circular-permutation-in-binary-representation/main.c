f68c465d7f6a61cdc325dd407d61b3ae2bbbbba3
Jay Xin <jayxin3002@qq.com>
2025-10-19 23:43:54 +0800

    Add 1238-circular-permutation-in-binary-representation.

---
+++ with-number/1238-circular-permutation-in-binary-representation/main.c
@@ -0,0 +1,70 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+// 1238. 循环 码排列 - 给你两个整数 n 和 start。你的任务是返回任意 (0,1,2,,...,2^n-1) 的排列 p ，并且满足：
+//  * p[0] = start
+//  * p[i] 和 p[i+1] 的二进制表示形式只有一位不同
+//  * p[0] 和 p[2^n -1] 的二进制表示形式也只有一位不同
+//  
+// 示例 1：
+// 输入：n = 2, start = 3
+// 输出：[3,2,0,1]
+// 解释：这个排列的二进制表示是 (11,10,00,01)
+//      所有的相邻元素都有一位是不同的，另一个有效的排列是 [3,1,0,2]
+// 示例 2：
+// 输入：n = 3, start = 2
+// 输出：[2,6,7,5,4,0,1,3]
+// 解释：这个排列的二进制表示是 (010,110,111,101,100,000,001,011)
+//  
+// 提示：
+//  * 1 <= n <= 16
+//  * 0 <= start < 2^n
+
+// #v1
+int* circularPermutation(int n, int start, int* returnSize) {
+  *returnSize = 1 << n;
+
+  int *ret = (int *)malloc(sizeof(int) * (*returnSize)), offset, i, tmp[*returnSize], j;
+
+  // gray code generation
+  for (i = 0; i < *returnSize; i++) {
+    tmp[i] = i ^ (i >> 1);
+    if (start == tmp[i]) offset = i;
+  }
+
+  offset = *returnSize - offset;
+
+  for (i = 0; i < *returnSize; i++) {
+    j = (i + offset) % *returnSize;
+    ret[j] = tmp[i];
+  }
+
+  return ret;
+}
+// #v1
+
+void print_1d_arr(int *arr, int n) {
+  if (!arr) {
+    putchar('\n');
+    return ;
+  }
+  for (int i = 0; i < n; i++) {
+    if (i) {
+      putchar(' ');
+    }
+    printf("%d", arr[i]);
+  }
+  printf("\n");
+}
+
+int main(int argc, char *argv[])
+{
+  int n, *ret, s, start;
+  scanf("%d%d", &n, &start);
+  ret = circularPermutation(n, start, &s);
+  print_1d_arr(ret, s);
+  return EXIT_SUCCESS;
+}
+
+/* vim: set fdm=marker fmr={,}: */

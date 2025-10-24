a0558f22aacdd831dd9247fcc54c865c0af75a07
Jay Xin <jayxin3002@qq.com>
2025-10-24 20:50:01 +0800

    Add 470-implement-rand10-using-rand7.

---
+++ with-number/470-implement-rand10-using-rand7/main.c
@@ -0,0 +1,49 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+// 470. 用 Rand7() 实现 Rand10() - 给定方法 rand7 可生成 [1,7] 范围内的均匀随机整 数，试写一个方法 rand10 生成 [1,10] 范围内的均匀随机整数。
+// 你只能调用 rand7() 且不能调用其他方法。请不要使用系统的 Math.random() 方法。
+// 每个测试用例将有一个内部参数 n，即你实现的函数 rand10() 在测试时将被调用的次数。 请注意，这不是传递给 rand10() 的参数。
+//  
+// 示例 1:
+// 输入: 1
+// 输出: [2]
+// 示例 2:
+// 输入: 2
+// 输出: [2,8]
+// 示例 3:
+// 输入: 3
+// 输出: [3,8,10]
+//  
+// 提示:
+//  * 1 &lt;= n &lt;= 105
+//  
+// 进阶:
+//  * rand7()调用次数的 期望值 [https://en.wikipedia.org/wiki/Expected_value] 是多少 ?
+//  * 你能否尽量少调用 rand7() ?
+
+// #v1
+// The rand7() API is already defined for you.
+// int rand7();
+// @return a random integer in the range 1 to 7
+
+int rand10() {
+  int r, c, i;
+
+  do {
+    r = rand7();
+    c = rand7();
+    i = c + (r - 1) * 7;
+  } while (i > 40);
+
+  return 1 + (i - 1) % 10;
+}
+// #v1
+
+int main(int argc, char *argv[])
+{
+  return EXIT_SUCCESS;
+}
+
+/* vim: set fdm=marker fmr={,}: */

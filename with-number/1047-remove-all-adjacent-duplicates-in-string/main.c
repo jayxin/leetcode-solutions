56c7064d56889238c666821419a0919b8a4ad8b3
Jay Xin <jayxin3002@qq.com>
2025-11-01 18:07:43 +0800

    Add 1047-remove-all-adjacent-duplicates-in-string.

---
+++ with-number/1047-remove-all-adjacent-duplicates-in-string/main.c
@@ -0,0 +1,52 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// 1047. 删除字符串中的所有相邻重复项 - 给出由小写字母组成的字符串 s，重复项 删除操作会选择两个相邻且相同的字母，并删除它们。
+// 在 s 上反复执行重复项删除操作，直到无法继续删除。
+// 在完成所有重复项删除操作后返回最终的字符串。答案保证唯一。
+//  
+// 示例：
+// 输入："abbaca"
+// 输出："ca"
+// 解释：
+// 例如，在 "abbaca" 中，我们可以删除 "bb" 由于两字母相邻且相同 ，这是此时唯一可以执行删除操作的重复项。之后我们得到字符串 "aaca"，其中又只有 "aa" 可以执行重复项删除操作，所以最后的字符串为 "ca"。
+//  
+// 提示：
+//  1. 1 <= s.length <= 105
+//  2. s 仅由小写英文字母组成。
+
+// #v1
+char* removeDuplicates(char* s) {
+  int n = strlen(s), top = -1;
+  char *stack = calloc(n + 1, sizeof(char));
+
+  while (*s) {
+    if (top >= 0 && stack[top] == *s) { // stack is not empty
+      // pop
+      --top;
+    } else {
+      // push
+      stack[++top] = *s;
+    }
+
+    ++s;
+  }
+
+  // string end
+  stack[++top] = '\0';
+
+  return stack;
+}
+// #v1
+
+int main(int argc, char *argv[])
+{
+  char s[100];
+  scanf("%s", s);
+  printf("%s\n", removeDuplicates(s));
+  return EXIT_SUCCESS;
+}
+
+/* vim: set fdm=marker fmr={,}: */

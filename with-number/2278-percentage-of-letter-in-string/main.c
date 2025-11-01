bbbddfae03c1543e825566033735bea5c4342930
Jay Xin <jayxin3002@qq.com>
2025-11-01 09:57:19 +0800

    Add 2278-percentage-of-letter-in-string.

---
+++ with-number/2278-percentage-of-letter-in-string/main.c
@@ -0,0 +1,48 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+// 2278. 字母在字符串中的百分比 - 给你一个字符串 s 和一个字符 letter ，返回在 s 中 等于 letter 字符所占的 百分比 ，向下取整到最接近的百分比。
+//  
+// 示例 1：
+// 输入：s = "foobar", letter = "o"
+// 输出：33
+// 解释：
+// 等于字母 'o' 的字符在 s 中占到的百分比是 2 / 6 * 100% = 33% ，向下取整 ，所以返回 33 。
+// 示例 2：
+// 输入：s = "jjjj", letter = "k"
+// 输出：0
+// 解释：
+// 等于字母 'k' 的字符在 s 中占到的百分比是 0% ，所以返回 0 。
+//  
+// 提示：
+//  * 1 <= s.length <= 100
+//  * s 由小写英文字母组成
+//  * letter 是一个小写英文字母
+
+// #v1
+int percentageLetter(char* s, char letter) {
+  int len = 0, cnt = 0;
+
+  while (*s) {
+    if (*s == letter) ++cnt;
+
+    ++s;
+    ++len;
+  }
+
+  return 100 * cnt / len;
+}
+// #v1
+
+int main(int argc, char *argv[])
+{
+  char c;
+  char s[100];
+  scanf("%c\n", &c);
+  scanf("%s", s);
+  printf("%d\n", percentageLetter(s, c));
+  return EXIT_SUCCESS;
+}
+
+/* vim: set fdm=marker fmr={,}: */

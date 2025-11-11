677d5aa8b8169d62fef0a2f5e3ec97261e91d400
Jay Xin <jayxin3002@qq.com>
2025-11-11 14:05:05 +0800

    Add 2644-find-the-maximum-divisibility-score.

---
+++ with-number/2644-find-the-maximum-divisibility-score/main.c
@@ -0,0 +1,86 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// 2644. 找出可整除性得分最大的整数 - 给你两个整数数组 nums 和 divisors 。
+// divisors[i] 的 可整除性得分 等于满足 nums[j] 能被 divisors[i] 整除的下标 j 的数量。
+// 返回 可整除性得分 最大的整数 divisors[i] 。如果有多个整数具有最大得分，则返回数值最小的一个。
+//  
+// 示例 1：
+// 输入：nums = [2,9,15,50], divisors = [5,3,7,2]
+// 输出：2
+// 解释：
+// divisors[0] 的可整除性分数为 2 因为 nums[2] 和 nums[3] 能被 5 整除。
+// divisors[1] 的可整除性分数为 2 因为 nums[1] 和 nums[2] 能被 3 整除。
+// divisors[2] 的可整除性分数为 0 因为 nums 中没有数字能被 7 整除。
+// divisors[3] 的可整除性分数为 2 因为 nums[0] 和 nums[3] 能够被 2 整除。
+// 因为 divisors[0] 、divisor[1] 和 divisors[3] 有相同的可整除性分数，我们返回更小的那个 divisors[3]。
+// 示例 2：
+// 输入：nums = [4,7,9,3,9], divisors = [5,2,3]
+// 输出：3
+// 解释：
+// divisors[0] 的可整除性分数为 0 因为 nums 中没有数字能被 5 整除。
+// divisors[1] 的可整除性分数为 1 因为只有 nums[0] 能被 2 整除。
+// divisors[2] 的可整除性分数为 3 因为 nums[2] ，nums[3] 和 nums[4] 能被 3 整除。
+// 示例 3：
+// 输入：nums = [20,14,21,10], divisors = [10,16,20]
+// 输出：10
+// 解释：
+// divisors[0] 的可整除性分数为 2 因为 nums[0] 和 nums[3] 能被 10 整除。
+// divisors[1] 的可整除性分数为 0 因为 nums 中没有数字能被 16 整除。
+// divisors[2] 的可整除性分数为 1 因为 nums[0] 能被 20 整除。
+// 因为 divisors[0] 的可整除性分数最大，我们返回 divisors[0]。
+//  
+// 提示：
+//  * 1 <= nums.length, divisors.length <= 1000
+//  * 1 <= nums[i], divisors[i] <= 109
+
+// #v1
+int maxDivScore(int* nums, int numsSize, int* divisors, int divisorsSize) {
+  int cnts[divisorsSize];
+
+  memset(cnts, 0, sizeof(cnts));
+
+  for (int i = 0; i < divisorsSize; i++) {
+    for (int j = 0; j < numsSize; j++) {
+      if (nums[j] % divisors[i] == 0) ++cnts[i];
+    }
+  }
+
+  int ret = divisors[0], c = cnts[0];
+
+  for (int i = 1; i < divisorsSize; i++) {
+    if (cnts[i] > c) {
+      ret = divisors[i];
+      c = cnts[i];
+    } else if (cnts[i] == c && divisors[i] < ret) {
+      ret = divisors[i];
+    }
+  }
+
+  return ret;
+}
+// #v1
+
+// read 1d array
+int* read_1d_arr(int n) {
+  int *ret = (int *)malloc(sizeof(int) * n);
+  for (int i = 0; i < n; i++) {
+    scanf("%d", ret + i);
+  }
+  return ret;
+}
+
+int main(int argc, char *argv[])
+{
+  int n0, n1, *a0, *a1;
+  scanf("%d", &n0);
+  a0 = read_1d_arr(n0);
+  scanf("%d", &n1);
+  a1 = read_1d_arr(n1);
+  printf("%d\n", maxDivScore(a0, n0, a1, n1));
+  return EXIT_SUCCESS;
+}
+
+/* vim: set fdm=marker fmr={,}: */

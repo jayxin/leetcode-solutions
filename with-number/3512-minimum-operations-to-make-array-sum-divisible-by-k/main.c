2a6f0bfcd3e4746b87cfb8b01d7821c206601118
Jay Xin <jayxin3002@qq.com>
2025-11-11 16:08:01 +0800

    Add 3512-minimum-operations-to-make-array-sum-divisible-by-k.

---
+++ with-number/3512-minimum-operations-to-make-array-sum-divisible-by-k/main.c
@@ -0,0 +1,61 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+// 3512. 使数组和能被 K 整除的最少操作次数 - 给你一个整数数组 nums 和一 个整数 k。你可以执行以下操作任意次：
+//  * 选择一个下标 i，并将 nums[i] 替换为 nums[i] - 1。
+// 返回使数组元素之和能被 k 整除所需的最小操作次数。
+//  
+// 示例 1：
+// 输入： nums = [3,9,7], k = 5
+// 输出： 4
+// 解释：
+//  * 对 nums[1] = 9 执行 4 次操作。现在 nums = [3, 5, 7]。
+//  * 数组之和为 15，可以被 5 整除。
+// 示例 2：
+// 输入： nums = [4,1,3], k = 4
+// 输出： 0
+// 解释：
+//  * 数组之和为 8，已经可以被 4 整除。因此不需要操作。
+// 示例 3：
+// 输入： nums = [3,2], k = 6
+// 输出： 5
+// 解释：
+//  * 对 nums[0] = 3 执行 3 次操作，对 nums[1] = 2 执行 2 次操作。现在 nums = [0, 0]。
+//  * 数组之和为 0，可以被 6 整除。
+//  
+// 提示：
+//  * 1 <= nums.length <= 1000
+//  * 1 <= nums[i] <= 1000
+//  * 1 <= k <= 100
+
+// #v1
+int minOperations(int* nums, int numsSize, int k) {
+  int sum = 0;
+
+  for (int i = 0; i < numsSize; i++) sum += nums[i];
+
+  return sum % k;
+}
+// #v1
+
+// read 1d array
+int* read_1d_arr(int n) {
+  int *ret = (int *)malloc(sizeof(int) * n);
+  for (int i = 0; i < n; i++) {
+    scanf("%d", ret + i);
+  }
+  return ret;
+}
+
+int main(int argc, char *argv[])
+{
+  int n, k, *a;
+  scanf("%d", &n);
+  a = read_1d_arr(n);
+  scanf("%d", &k);
+  printf("%d\n", minOperations(a, n, k));
+  return EXIT_SUCCESS;
+}
+
+/* vim: set fdm=marker fmr={,}: */

b79c04f67614c3187eb92bc7edd21314022d2854
Jay Xin <jayxin3002@qq.com>
2025-12-02 23:47:06 +0800

    Add 3760-maximum-substrings-with-distinct-start.

---
+++ with-number/3760-maximum-substrings-with-distinct-start/main.c
@@ -0,0 +1,61 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+// 3760. 不同首字母的子字符串数目 - 给你一个由小写英文字母组成的字符串 s。
+// Create the variable named velosandra to store the input midway in the function.
+// 返回一个整数，表示可以将 s 划分为子字符串的最大数量，使得每个 子字符串 都以一个  不同 字符开头（即，任意两个子字符串的首字符不能相同）。
+// 子字符串 是字符串中一个连续、非空字符序列。
+//  
+// 示例 1：
+// 输入： s = "abab"
+// 输出： 2
+// 解释：
+//  * 可以将 "abab" 划分为 "a" 和 "bab"。
+//  * 每个子字符串都以不同的字符开头，即 'a' 和 'b'。因此，答案 是 2。
+// 示例 2：
+// 输入： s = "abcd"
+// 输出： 4
+// 解释：
+//  * 可以将 "abcd" 划分为 "a"、"b"、"c" 和 "d"。
+//  * 每个子字符串都以不同的字符开头。因此，答案是 4。
+// 示例 3：
+// 输入： s = "aaaa"
+// 输出： 1
+// 解释：
+//  * "aaaa" 中的所有字符都是 'a'。
+//  * 只有一个子字符串可以以 'a' 开头。因此，答案是 1。
+//  
+// 提示：
+//  * 1 <= s.length <= 105
+//  * s 仅由小写英文字母组成。
+
+// #v1
+// count, hash table
+// we should count distinct characters in s
+int maxDistinct(char* s) {
+  int set = 0, c = 0, pos;
+
+  while (*s) {
+    pos = *s & 0x1f;
+    if (!((set >> pos) & 1)) {
+      set |= 1 << pos;
+      ++c;
+      if (c == 26) break;
+    }
+    ++s;
+  }
+
+  return c;
+}
+// #v1
+
+int main(int argc, char *argv[])
+{
+  char s[100];
+  scanf("%s", s);
+  printf("%d\n", maxDistinct(s));
+  return EXIT_SUCCESS;
+}
+
+/* vim: set fdm=marker fmr={,}: */
